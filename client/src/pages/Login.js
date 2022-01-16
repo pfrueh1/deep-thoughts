a6e7a83a4fde890fd58f5ba07e33e1a3e8b3d0b7
pfrueh1 <fruehpaul@gmail.com>
2022-01-16 09:28:03 -0500

    login related mutations

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -14,14 +18,24 @@ const Login = (props) => {
   };
 
   // submit form
-  const handleFormSubmit = async (event) => {
+// submit form
+  const handleFormSubmit = async event => {
     event.preventDefault();
 
-    // clear form values
+    try {
+      const { data } = await login({
+        variables: { ...formState }
+      });
+    
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
     setFormState({
       email: '',
       password: '',
-    });
+    })
   };
 
   return (
@@ -53,6 +67,7 @@ const Login = (props) => {
                 Submit
               </button>
             </form>
+            {error && <div>Login failed</div>}
           </div>
         </div>
       </div>
